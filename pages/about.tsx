ada0fc9f2e332b2b03ce28264776fcdb5aa090c3
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-05-11 16:52:43 +0000

    🔥

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
 import { MainLayout } from "@/src/components/Layout/mainlayout";
 import { Index } from "@/src/components/pages";
 import { About } from "@/src/components/pages/about";
-import Head from "next/head";
 
 const Home = () => {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { MainLayout } from "@/src/components/Layout/mainlayout";
 import { Index } from "@/src/components/pages";
 import { NextPage } from "next";
-import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
