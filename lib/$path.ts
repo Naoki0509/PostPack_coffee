2a68705091e86dcd629131ba68d3e4df4185f32b
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-05-11 16:49:40 +0000

    💡

--- lib/$path.ts
+++ lib/$path.ts
@@ -1,26 +1,14 @@
 export const pagesPath = {
-  about: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: "/about" as const,
-      hash: url?.hash,
-    }),
+  "about": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
   },
-  login: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: "/login" as const,
-      hash: url?.hash,
-    }),
+  "login": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/login' as const, hash: url?.hash })
   },
-  signup: {
-    $url: (url?: { hash?: string }) => ({
-      pathname: "/signup" as const,
-      hash: url?.hash,
-    }),
+  "signup": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/signup' as const, hash: url?.hash })
   },
-  $url: (url?: { hash?: string }) => ({
-    pathname: "/" as const,
-    hash: url?.hash,
-  }),
-};
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
 
-export type PagesPath = typeof pagesPath;
+export type PagesPath = typeof pagesPath
