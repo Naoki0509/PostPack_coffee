c9c7fa4b2d87b824c344c04fc08c173ad4c80678
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-05-29 01:26:46 +0900

    add code

--- lib/firebase/index.tsx
+++ lib/firebase/index.tsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
 import { initializeApp, FirebaseApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
+import {
+  User,
+  createUserWithEmailAndPassword,
+  getAuth,
+  initializeAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useRouter } from "next/router";
+import { createContext, useState } from "react";
+import { set } from "react-hook-form";
+import nookies from "nookies";
 
+export const GlobalContext =
+  createContext<ReturnType<typeof useGlobalViewModel>>(null);
 
-export const GolobalViewModel = () => {
-  const Inithalize = () => {
+export const useGlobalViewModel = () => {
+  const [firebase, setFirebase] = useState<{
+    firebase?: FirebaseApp;
+    firestore?: Firestore;
+  }>({
+    firebase: null,
+    firestore: null,
+  });
+
+  const [settionUser, useSessionUser] = useState<User>(null);
+  const initialize = () => {
     const firebase = initializeApp({
       apiKey: "AIzaSyC1ji6iKlfZEN6gpmLsWOXfwZQ0N5T5DZY",
       authDomain: "postpackcoffee.firebaseapp.com",
@@ -13,5 +35,61 @@ export const GolobalViewModel = () => {
       appId: "1:773391630646:web:a6f75f5d49536f3062f397",
       measurementId: "G-Y2K0SEJVP6",
     });
+
+    const db = getFirestore(firebase);
+    setFirebase({
+      firebase,
+      firestore: db,
+    });
+  };
+
+  const [LoadingLogin, setLoadingLogin] = useState(false);
+  const [Error, setError] = useState(false);
+
+  const SignUp = async (email: string, password: string) => {
+    setLoadingLogin(true);
+    if (!firebase.firebase) return;
+    const auth = getAuth();
+    auth.languageCode = "ja";
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if (userCredential != null && userCredential?.user) {
+          useSessionUser(userCredential.user);
+          nookies.set(null, "token", userCredential.user.refreshToken, {
+            maxAge: 30 * 24 * 60 * 60,
+            path: "/",
+          });
+        }
+        setLoadingLogin(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoadingLogin(false);
+      });
+  };
+
+  const SignIn = async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      return user;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const SignOut = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    ...GlobalViewModel,
   };
 };
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,34 @@
+import { GlobalViewModel } from "@/lib/firebase";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
 import { NextPage } from "next";
+import { FormEventHandler } from "react";
+import { useForm } from "react-hook-form";
 
-const Signup: NextPage = () => {
-  return <div>Signup</div>;
+type Props = {
+  email: string;
+  password: string;
 };
 
-export default Signup;
+const SignupPage: NextPage = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: { email: "", password: "" },
+    values: { email: "", password: "" },
+  });
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextInput label="Email" placeholder="Email" {...register("email")} />
+        <PasswordInput
+          label="Password"
+          placeholder="Password"
+          {...register("password")}
+        />
+
+        <Button type="submit">登録</Button>
+      </form>
+    </>
+  );
+};
+
+export default SignupPage;
